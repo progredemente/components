870f3f7dc096d2ad3f4c442abfbce250f92dc9f3
progredemente <progredemente@gmail.com>
2024-02-14 22:40:35 +0000

    Test

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
-          exclude: '../src/gifer/createGif.js'
+          exclude: /\/src\/gifer\/createGif.js/
         }
       })
     ]
