a51d9f86d3e2b1ac95a0609749dc12d243f85eef
progredemente <progredemente@gmail.com>
2024-02-14 23:57:36 +0000

    Test

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
+            presets: ['@babel/preset-react', '@babel/preset-env', { "exclude": ["transform-function-name"] }],
             plugins: ['@babel/plugin-transform-runtime'],
           },
         },
