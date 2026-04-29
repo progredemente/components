1c609191e53a4d591a683f13a68be5438836bcfb
progredemente <progredemente@gmail.com>
2026-04-29 15:59:21 +0200

    style.css serving fix

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 const webpack = require('webpack');
@@ -46,6 +47,14 @@ const prodConfig = {
       filename: 'fonts.css',
       mediaUrl: process.env.MEDIA_URL,
       inject: false
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './public/style.css',
+          to: 'style.css'
+        }
+      ]
     }),
     new webpack.DefinePlugin({
       "process.env": JSON.stringify(process.env),
